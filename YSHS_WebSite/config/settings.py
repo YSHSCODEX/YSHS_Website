fa5491d7cdf419ff363b8c556e3b1cac1f197daf
ILikeForest <namu90541@gmail.com>
2024-08-23 23:31:17 +0900

    make superuser(id:CODEX_admin, password:google계정 비밀번호). mainPage에서 로그인하면 로그인페이지로 이동. 로그인 성공시 mainPage로 이동.

--- YSHS_WebSite/config/settings.py
+++ YSHS_WebSite/config/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
