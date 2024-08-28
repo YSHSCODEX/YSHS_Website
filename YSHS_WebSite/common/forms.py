1ecbb6df18b5b9323d8f540ad4c8ff590431243a
ILikeForest <namu90541@gmail.com>
2024-08-28 23:34:59 +0900

    커스텀한 모델 가지고 폼 만듬

--- YSHS_WebSite/common/forms.py
+++ YSHS_WebSite/common/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForms
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
+class CustomUserForm(UserCreationForm):
+    grade = forms.ChoiceField(choices=[(i, f'{i}st grade') for i in range(1, 4)], label='학년')
+    classNum = forms.ChoiceField(choices=[(i, f'class {i}') for i in range(1, 10)], label='반')
+    studentNum = forms.ChoiceField(choices=[(i, f'number {i}') for i in range(1, 40)], label='출석번호')
+    birthDate = forms.DateField(label='생년월일', widget=forms.SelectDateWidget(years=range(1900, 2100)))
+    email = forms.EmailField(label='개인 이메일 주소', required=False)
 
-class UserForm(UserCreationForms):
-    
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('grade', 'classNum', 'studentNum', 'birthDate', 'email')
