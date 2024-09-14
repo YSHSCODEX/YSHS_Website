135b3b1ede1a9369cf8eff105d7f42e38d411048
ILikeForest <namu90541@gmail.com>
2024-09-14 21:53:06 +0900

    회원가입 폼 작동함. 왜지?

--- YSHS_WebSite/common/forms.py
+++ YSHS_WebSite/common/forms.py
@@ -3,12 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class CustomUserForm(UserCreationForm):
+    '''
     grade = forms.ChoiceField(choices=[(i, f'{i}st grade') for i in range(1, 4)], label='학년')
     classNum = forms.ChoiceField(choices=[(i, f'class {i}') for i in range(1, 10)], label='반')
     studentNum = forms.ChoiceField(choices=[(i, f'number {i}') for i in range(1, 40)], label='출석번호')
     birthDate = forms.DateField(label='생년월일', widget=forms.SelectDateWidget(years=range(1900, 2100)))
     email = forms.EmailField(label='개인 이메일 주소', required=False)
-
+    # username 아이디
+    # password1 비밀번호1
+    # password2 비밀번호 확인
+    '''
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('grade', 'classNum', 'studentNum', 'birthDate', 'email')
--- YSHS_WebSite/common/views.py
+++ YSHS_WebSite/common/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # type: ignore
+from django.contrib.auth import login, authenticate
+from .forms import CustomUserForm
+from .models import CustomUser
 
 def signup(request):
     return render(request, 'mainPage/home.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserForm(request.POST) # CustomUserForm 사용할거임
+        if form.is_valid():
+            user = form.save()  # 사용자 생성
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)   # 사용자 인증
+            if user is not None:
+                login(request, user)    # 로그인 처리
+                return redirect('mainPage:home')
+    else:
+        form = CustomUserForm()
+    
+    return render(request, 'common/signUp.html', {'form' : form})
