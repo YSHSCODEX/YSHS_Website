e862f148b029a1b04083d27a95bcfb0498759f1d
jayenyeo <jayen.yeo.sb@gmail.com>
2024-11-24 00:24:36 +0900

    나름 재밌네

--- site/board/urls.py
+++ site/board/urls.py
@@ -3,5 +3,5 @@ from . import views #뷰를 url에 연결
 
 urlpatterns = [
     path('', views.question_list, name = 'question_list'), #질문 목록
-    path('<int:question_id>/', views.question_deatil, name='question_detail'), #질문 상세
+    path('<int:question_id>/', views.question_detail, name='question_detail'), #질문 상세
 ]
