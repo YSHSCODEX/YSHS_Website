5c340322575661412e222f1036f08616774a19b4
support1415 <jiwony1027@gmail.com>
2024-12-26 20:30:39 +0900

    댓글

--- site/board/views.py
+++ site/board/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
 
-# Create your views here.
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = post.comments.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', post_id=post_id)
+        
+    else:
+        form = CommentForm()
+
+    return render(request, 'board/post_detail.html', {
+        'post': post,
+        'comments': commments,
+        'form': form,
+    })
+
