a6d38b2e6f897d7008fa8a863bfd1babd1d5ff9a
support1415 <jiwony1027@gmail.com>
2024-12-26 20:29:00 +0900

    댓글

--- site/board/models.py
+++ site/board/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class Comment(models.Model):
+    article = models.ForeignKey(on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.content
